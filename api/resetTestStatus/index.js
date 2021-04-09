91f99bf8d1257e931d423e5ba7ff634b9efa039f
Andre Essing <21235652+aessing@users.noreply.github.com>
2021-04-09 12:03:08 +0200

    Switch to HTTP Trigger

--- api/resetTestStatus/index.js
+++ api/resetTestStatus/index.js
@@ -1,15 +1,37 @@
 const tableStore = require(`azure-storage`);
 
-module.exports = async function (context, resetTestStatusTimer) {
+module.exports = async function (context) {
   let classes = await getClasses();
   let responseStatus = await resetTestStatus(classes);
+
+  let responseMessage;
+
+  console.log(responseStatus);
+
+  switch (responseStatus) {
+    case 200:
+      responseMessage = `SUCCESS: Reset status of all classes`;
+      break;
+    case 500:
+      responseMessage = `ERROR: Couldn't update status of sll classes`;
+      break;
+    default:
+      responseMessage = `UNDEFINED: Unknown error`;
+  }
+
+  console.log(responseMessage);
+
+  context.res = {
+    status: responseStatus,
+    body: responseMessage,
+  };
 };
 
 async function getClasses() {
   const tableService = tableStore.createTableService();
   const statusQuery = new tableStore.TableQuery().select(`PartitionKey`);
 
-  return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve) {
     tableService.queryEntities(
       process.env["STATUSTABLE_NAME"],
       statusQuery,
@@ -24,8 +46,6 @@ async function getClasses() {
           });
 
           resolve(resultSet);
-        } else {
-          reject(error.message);
         }
       }
     );
@@ -35,22 +55,31 @@ async function getClasses() {
 async function resetTestStatus(classList) {
   const tableService = tableStore.createTableService();
 
-  classList.forEach(function (entity) {
-    const updatedStatus = {
-      PartitionKey: {
-        _: entity,
-      },
-      RowKey: {
-        _: "",
-      },
-      Status: {
-        _: 0,
-      },
-    };
-    tableService.replaceEntity(
-      process.env["STATUSTABLE_NAME"],
-      updatedStatus,
-      function (error) {}
-    );
+  return new Promise(function (resolve, reject) {
+    classList.forEach(function (entity) {
+      const updatedStatus = {
+        PartitionKey: {
+          _: entity,
+        },
+        RowKey: {
+          _: "",
+        },
+        Status: {
+          _: 0,
+        },
+      };
+
+      tableService.replaceEntity(
+        process.env["STATUSTABLE_NAME"],
+        updatedStatus,
+        function (error) {
+          if (!error) {
+            resolve(200);
+          } else {
+            reject(500);
+          }
+        }
+      );
+    });
   });
 }
